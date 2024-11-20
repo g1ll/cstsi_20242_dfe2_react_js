a38418dbf70734278fd4e7470d24a60ffbe3f78d
gill from r5950x <gillvelleda@gmail.com>
2024-11-20 18:28:09 -0300

    feat: exemplo projeto com pre-processador less

---
+++ topico_05/exemplos/04-vite-react-less-css/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import cssLogo from "/img/css3.png";
+// import "../public/css/style.css";
+import './styles/less/style.less'
+// import "./App.css";
+import "./styles/less/App.less";
+// import "./styles/less/paragraphs.less"
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a
+          href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+          target="_blank"
+        >
+          <img src={cssLogo} className="logo css_logo" alt="CSS logo" />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div className="dev"></div>
+    </>
+  );
+}
+
+export default App;
