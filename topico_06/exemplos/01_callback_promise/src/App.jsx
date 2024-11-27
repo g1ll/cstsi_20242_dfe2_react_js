7ed9e398445e613d6f4ed933e9d4cd0175673820
gill from r5950x <gillvelleda@gmail.com>
2024-11-26 23:41:42 -0300

    feat: callback versus promises

---
+++ topico_06/exemplos/01_callback_promise/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const funcCallback = (text, callback) => {
+    setTimeout(() => {
+      console.log(text);
+      callback && callback();
+    }, 500);
+  };
+
+  const funcPromise = (text) =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(text);
+        resolve(funcPromise);
+      }, 500);
+    });
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Callback vs Promise</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            funcCallback("Hello", () => {
+              funcCallback("World", () => {
+                funcCallback("CallBack",()=>{
+                  funcCallback("Hell");
+                });
+              });
+            });
+          }}
+        >
+          CallBack Hell
+        </button>
+        <button
+          onClick={() =>
+            funcPromise("Hello")
+              .then(fun => fun("World"))
+              .then(fun => fun("Promise"))
+              .then(fun => fun("Chaning"))
+          }
+        >
+          Promise Chaning
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
