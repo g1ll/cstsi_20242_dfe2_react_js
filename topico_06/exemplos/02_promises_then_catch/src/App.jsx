ed72de028a2d74ac3e0aef0df58b715a8acbcfa0
gill from r5950x <gillvelleda@gmail.com>
2024-12-02 20:49:24 -0300

    feat: exemplo objeto promise com then e catch. Capturando erros e promises rejeitadas com o catch.

---
+++ topico_06/exemplos/02_promises_then_catch/src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const promessa = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve('Promessa cumprida!!!')
+    },1000)
+  
+    setTimeout(() => {
+      reject('Promessa rejeitada!!!') // lançando um erro
+    },500);
+  })
+
+  useEffect(() => {
+    promessa.then(data=>console.log(data))
+      .catch(error=>console.error(error)) //capturando o erro
+      .finally(()=>console.log('Finalizado!!!'))
+  },[])
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
