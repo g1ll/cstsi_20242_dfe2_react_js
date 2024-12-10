885f2a3d012fd62af6b89425828a9669c47ad533
g1ll <gillvelleda@gmail.com>
2024-12-10 14:45:49 -0500

    feat: modal de remoção de um produto.

---
+++ topico_07/01_dashboard_crud_produtos_api/src/pages/Dashboard/Produtos/ModalRemoveProdutoForm.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const FormContainer = styled.div`
+
+`
+
+export const Buttons = styled.div`
+        margin: 10px;
+        /* border: solid 2px blue; */
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+`
