5383146e25c6dfa5fc42d67d067c19d8cc4b93a2
gill from r5950x <gillvelleda@gmail.com>
2024-12-11 15:30:14 -0300

    feat: configuração de rotas para os modais de criação e edição.

--- topico_07/03_dashboard_routing_modals/src/config/routes.jsx
+++ topico_07/03_dashboard_routing_modals/src/config/routes.jsx
@@ -9,10 +9,11 @@ import Show from "../pages/Show/Show";
 import Dash from "../layouts/Dash";
 import Login from "../pages/Login/Login";
 import Cadastro from "../pages/Cadastro/Cadastro";
-// import Dashboard from "../pages/Dashboard/Dashboard";
 import Users from "../pages/Users/Users";
 import Logout from "../pages/Logout/Logout";
 import Produtos from "../pages/Dashboard/Produtos/Produtos";
+import ModalAddProduto from "../pages/Dashboard/Produtos/ModalAddProduto";
+import ModalEditProduto from "../pages/Dashboard/Produtos/ModalEditProduto";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,10 +24,19 @@ const router = createBrowserRouter(
         <Route path="/login" element={<Login />} />
       </Route>
       <Route path="/" element={<Dash />}>
-        <Route path="/dashboard" element={<Produtos/>} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/logout" element={<Logout />} />
+        <Route path="dashboard" element={<Produtos/>} >
+          <Route path="produtos" >
+            <Route path="create" element={
+                <ModalAddProduto/>
+              } />
+              <Route path="edit/:id" element={
+                <ModalEditProduto/>
+              } />
+          </Route>
+          <Route path="users" element={<Users />} />
+          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="logout" element={<Logout />} />
+        </Route>
       </Route>
     </>
   )
