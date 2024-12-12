1c1a65ba1e34da342b9f997b50d63510610d018f
Prof. Gill Gonzales <gillvelleda@gmail.com>
2024-12-12 11:28:22 -0300

    fix: correções no link para a página inicial

--- topico_07/03_dashboard_routing_modals/src/layouts/Dash.jsx
+++ topico_07/03_dashboard_routing_modals/src/layouts/Dash.jsx
@@ -37,7 +37,7 @@ export default function Dash() {
     <DefaultStyled>
         <header>
           <DefaultLogo>
-            <Link href="/">
+            <Link to="/" target="_blank">
               <img src={appLogo} />
             </Link>
           </DefaultLogo>
